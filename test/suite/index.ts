c4eaea15d71bb04998c57f9940ce29f002f40126
zawys <pj94ye@runbox.com>
2021-03-21 00:00:00 +0000

    change TODO

--- test/suite/index.ts
+++ test/suite/index.ts
@@ -1,6 +1,5 @@
 import { runTest as runUnitTests } from "./unitTest.systemTest";
-// TODO [2021-03-01] Disable the test which requires
-// stable Code to be installed because of a packaging bug with Code.
+// TODO [2021-05-01] Rewrite unstashConflictTests for the main branch.
 // import { runTest as runUnstashConflictTest } from "./unstashConflict.systemTest";
 
 export async function runTests(): Promise<boolean> {
